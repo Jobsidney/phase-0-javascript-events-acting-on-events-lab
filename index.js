aaa1e57b17fb29a59f3fc2eb7e7e21b9c3ffd620
Jobsidney <jobsidney67@gmail.com>
2022-06-04 08:10:13 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+function moveDodgerRight() {
+    const leftNum=dodger.style.left.replace("px","");
+    const left=parseInt(leftNum,10);
+    if (left>0) {
+        dodger.style.left=`${left+1}px`;}
+    
+};
+
+
+function moveDodgerLeft() {
+    const leftNum=dodger.style.left.replace("px","");
+    const left=parseInt(leftNum,10);
+    if (left>0) {
+        dodger.style.left=`${left-1}px`;}
+    
+};
+
+document.addEventListener("keydown",function(event){
+    if (event.key==="arrowRight"){
+        moveDodgerRigt();
+    }else if (event.key==="arrowLeft"){moveDodgerLeft()}
+})
+
